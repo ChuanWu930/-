b02158f1c9df3fa9d7dd8c477ad40c64d999d861
ChuanWu930 <107868610+ChuanWu930@users.noreply.github.com>
2024-06-06 21:05:46 +0800

    Create 1.html.php

---
+++ 1.html.php
@@ -0,0 +1,12 @@
+<html> <!-- 定義 HTML 文件的根元素 -->
+    <head> <!-- 定義文件的頭部，包含元數據和標題等信息 -->
+        <title>html+php</title> <!-- 定義網頁標題，顯示在瀏覽器標籤上 -->
+    </head>      
+    <body> <!-- 定義文件的主體內容 -->
+        <?php echo "Hello world!"; ?> <!-- 使用 PHP 輸出 "Hello world!" 字符串 -->
+        <h1>html</h1> <!-- 定義一個 HTML 標題，標籤等級為 1 -->
+        <?php echo "Hello world!"; ?> <!-- 再次使用 PHP 輸出 "Hello world!" 字符串 -->
+        <h1>html</h1> <!-- 定義另一個 HTML 標題，標籤等級為 1 -->
+        <?php echo "Hello world!"; ?> <!-- 第三次使用 PHP 輸出 "Hello world!" 字符串 -->
+    </body>
+</html> <!-- 結束 HTML 文件 -->
