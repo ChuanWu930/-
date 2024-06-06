9143c2e1022d483755087fb3d0c5ea9708fb0fe0
ChuanWu930 <107868610+ChuanWu930@users.noreply.github.com>
2024-06-06 21:16:57 +0800

    Create 20.user_edit.php

---
+++ 20.user_edit.php
@@ -0,0 +1,20 @@
+<?php
+
+error_reporting(0); // 設置錯誤報告等級為 0，禁用錯誤報告
+session_start(); // 啟動會話或恢復先前的會話
+if (!$_SESSION["id"]) { // 如果會話中沒有 "id" 變數
+    echo "請登入帳號"; // 輸出請登入帳號的訊息
+    echo "<meta http-equiv=REFRESH content='3, url=2.login.html'>"; // 3秒後重新導向到 2.login.html 頁面
+}
+else{   
+    $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust"); // 建立資料庫連結
+    if (!mysqli_query($conn, "update user set pwd='{$_POST['pwd']}' where id='{$_POST['id']}'")){ // 更新資料庫中指定使用者的密碼
+        echo "修改錯誤"; // 輸出修改錯誤的訊息
+        echo "<meta http-equiv=REFRESH content='3, url=18.user.php'>"; // 3秒後重新導向到 18.user.php 頁面
+    }else{
+        echo "修改成功，三秒鐘後回到網頁"; // 輸出修改成功的訊息
+        echo "<meta http-equiv=REFRESH content='3, url=18.user.php'>"; // 3秒後重新導向到 18.user.php 頁面
+    }
+}
+
+?>
